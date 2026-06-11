61898fdbf918f712491118e6a35965ab5940ff75
Fahim2023 <fahimshahrier2023@gmail.com>
2026-06-12 01:34:50 +0600

    started the backend with node againn

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+const PORT = 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
