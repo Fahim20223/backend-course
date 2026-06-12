2e35ac63448fcee15c3b0c6821a74b38537d3991
Fahim2023 <fahimshahrier2023@gmail.com>
2026-06-13 01:13:31 +0600

    instead of require used the import of the express in server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/hello", (req, res) => {
-  res.json({ message: "Hello World" });
-});
+// app.get("/hello", (req, res) => {
+//   res.json({ message: "Hello World" });
+// });
+import express from "express";
+
+const app = express();
 
 const PORT = 5001;
 app.listen(PORT, () => {
