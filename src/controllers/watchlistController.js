3c20e56149d0e73de7d45ed99df1507ea22f24b3
Fahim2023 <fahimshahrier2023@gmail.com>
2026-07-20 18:47:49 +0600

    added the first 2 condition's for verification of movie exists

--- src/controllers/watchlistController.js
+++ src/controllers/watchlistController.js
@@ -1,3 +1,14 @@
+import { Prisma } from "../config/db";
+
 const addToWatchList = async (req, res) => {
   const { movieId, status, rating, notes } = req.body;
+
+  // verify movie exists
+  const movie = await Prisma.movie.findUnique({
+    where: { id: movieId },
+  });
+
+  if (!movie) {
+    return res.status(404).json({ error: "Movie not found" });
+  }
 };
