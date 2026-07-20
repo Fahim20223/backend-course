bfbec059cd46654370a5de43d375220c532df68c
Fahim2023 <fahimshahrier2023@gmail.com>
2026-07-21 01:07:44 +0600

    added the check of already existing's in the watchlistcontroller

--- src/controllers/watchlistController.js
+++ src/controllers/watchlistController.js
@@ -1,14 +1,19 @@
-import { Prisma } from "../config/db";
+import { prisma } from "../config/db";
 
 const addToWatchList = async (req, res) => {
   const { movieId, status, rating, notes } = req.body;
 
   // verify movie exists
-  const movie = await Prisma.movie.findUnique({
+  const movie = await prisma.movie.findUnique({
     where: { id: movieId },
   });
 
   if (!movie) {
     return res.status(404).json({ error: "Movie not found" });
   }
+
+  // check it already added
+  const existingInsWatchList = await prisma.watchlistItem.findUnique({
+    where: { id: movieId },
+  });
 };
